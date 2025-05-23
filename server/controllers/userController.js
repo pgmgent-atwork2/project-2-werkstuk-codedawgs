72c4844ceeb8797ce7da9fdb87c2baf857802a70
mauroongena <mauro.ongena@icloud.com>
2025-05-23 14:41:34 +0200

    Implement postUser function to add new users to the database

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+export const postUser = async (req, res) => {
+  const { first_name, last_name } = req.body;
+  const token = uuidv4().slice(0, 13).replace("-", "");
+  try {
+    await knex("users").insert({
+      first_name,
+      last_name,
+      token,
+    });
+    res.redirect("/admin/gebruikers");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Gebruiker toevoegen mislukt.");
+  }
+};
