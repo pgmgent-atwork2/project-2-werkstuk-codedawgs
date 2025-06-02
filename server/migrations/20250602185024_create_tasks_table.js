954af6a5666d55a6ac0c147cf5504b06abca032d
mauroongena <mauro.ongena@icloud.com>
2025-06-02 20:51:00 +0200

    Added tasks table

---
+++ server/migrations/20250602185024_create_tasks_table.js
@@ -0,0 +1,22 @@
+const tableName = "tasks";
+
+export function up(knex) {
+  return knex.schema.createTable(tableName, function (table) {
+    table.increments("id").primary().unique();
+    table.string("title");
+    table.boolean("visible").notNullable();
+    table.boolean("completed").notNullable();
+    table
+      .string("interval")
+      .notNullable()
+      .checkIn(["daily", "weekly", "monthly"]);
+    table
+      .string("object_type")
+      .notNullable()
+      .checkIn(["department", "sub_department", "filter", "pump"]);
+    table.integer("object_id").unsigned();
+  });
+}
+export function down(knex) {
+  return knex.schema.dropTable(tableName);
+}
