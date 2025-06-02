e6737132feb1c07a513f5edcf49a8197694dd722
mauroongena <mauro.ongena@icloud.com>
2025-06-02 21:38:48 +0200

    Added tasks log relations

--- server/models/TasksLog.js
+++ server/models/TasksLog.js
@@ -1,5 +1,7 @@
 import knex from "../lib/Knex.js";
 import { Model } from "objection";
+import User from "./User.js";
+import Task from "./Task.js";
 
 Model.knex(knex);
 
@@ -28,6 +30,27 @@ class TasksLog extends Model {
         comment: { type: "string" },
         action: { type: "string" },
         task_date: { type: "date" },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: "task_logs.user_id",
+          to: "users.id",
+        },
+      },
+      task: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Task,
+        join: {
+          from: "task_logs.task_id",
+          to: "tasks.id",
+        },
       },
     };
   }
