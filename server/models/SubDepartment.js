0cb251339c69415a6918ca887f65ba606c4e7f5e
mauroongena <mauro.ongena@icloud.com>
2025-06-02 21:40:04 +0200

    Added sub department relations

--- server/models/SubDepartment.js
+++ server/models/SubDepartment.js
@@ -1,5 +1,9 @@
 import knex from "../lib/Knex.js";
 import { Model } from "objection";
+import Pump from "./Pump.js";
+import Filter from "./Filter.js";
+import Department from "./Department.js";
+import MeasurementLog from "./MeasurementLog.js";
 
 Model.knex(knex);
 
@@ -20,6 +24,44 @@ class SubDepartment extends Model {
         id: { type: "integer" },
         title: { type: "string", minLength: 1, maxLength: 255 },
         department_id: { type: "integer" },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      filters: {
+        relation: Model.HasManyRelation,
+        modelClass: Pump,
+        join: {
+          from: "sub_departments.id",
+          to: "pumps.sub_department_id",
+        },
+      },
+      filters: {
+        relation: Model.HasManyRelation,
+        modelClass: Filter,
+        join: {
+          from: "sub_departments.id",
+          to: "filters.sub_department_id",
+        },
+      },
+
+      department: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Department,
+        join: {
+          from: "sub_departments.department_id",
+          to: "departments.id",
+        },
+      },
+      measurement_log: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: MeasurementLog,
+        join: {
+          from: "sub_departments.id",
+          to: "measurement_logs.sub_department_id",
+        },
       },
     };
   }
