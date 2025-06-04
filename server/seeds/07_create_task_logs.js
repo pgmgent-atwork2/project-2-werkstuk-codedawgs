07b2906bca9e022230c98b05f12dd15f1ee00c82
mauroongena <mauro.ongena@icloud.com>
2025-06-04 15:24:35 +0200

    Fix: spelling error in task logs

--- server/seeds/07_create_task_logs.js
+++ server/seeds/07_create_task_logs.js
@@ -7,7 +7,7 @@ const seed = async function (knex) {
     task_id: 4,
     object_type: "pump",
     object_id: 4,
-    comment: "checked the pump very good",
+    comment: "checked pump very good",
     action: "completed",
     task_date: new Date("2025-04-17T03:24:00")
   },
