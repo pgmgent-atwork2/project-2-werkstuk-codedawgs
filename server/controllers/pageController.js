afff3e0a7cb188bbb4041006c993d9ed48b55c44
mauroongena <mauro.ongena@icloud.com>
2025-06-04 15:46:29 +0200

    Added task page controller

--- server/controllers/pageController.js
+++ server/controllers/pageController.js
@@ -23,3 +23,29 @@ export const userPage = async (req, res) => {
     res.status(500).send("Error while loading the users.");
   }
 };
+
+export const taskPage = async (req, res) => {
+  const { departmentString } = req.params;
+  const intervalString = req.path.split("/")[1];
+  try {
+    const tasks = await knex("tasks").select("*");
+    const departments = await knex("departments").select("*");
+    const sub_departments = await knex("sub_departments").select("*");
+    const filters = await knex("filters").select("*");
+    const pumps = await knex("pumps").select("*");
+
+    res.render("pages/taskpage", {
+      userRole: req.user.role,
+      tasks,
+      intervalString,
+      departmentString,
+      departments,
+      sub_departments,
+      filters,
+      pumps,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error loading tasks");
+  }
+};
