dba26a6ddf9ab048a3223620a4d4d2f0c1bb06d4
mauroongena <mauro.ongena@icloud.com>
2025-06-04 15:18:31 +0200

    Added create task logs seed

---
+++ server/seeds/07_create_task_logs.js
@@ -0,0 +1,17 @@
+const tableName = "task_logs";
+ 
+const seed = async function (knex) {
+  await knex(tableName).truncate();
+  await knex(tableName).insert([{ 
+    user_id: 2,
+    task_id: 4,
+    object_type: "pump",
+    object_id: 4,
+    comment: "checked the pump very good",
+    action: "completed",
+    task_date: new Date("2025-04-17T03:24:00")
+  },
+  ]);
+};
+ 
+export { seed };
