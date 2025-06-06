1f38f61d8e95397519701d87f98f87efbe6943cf
mauroongena <mauro.ongena@icloud.com>
2025-06-06 10:27:20 +0200

    Added basic styling for functionality purposes

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const tokenInput = document.querySelector(".register__token");
+  const pincodeInput = document.querySelector(".register__pincode");
+  const imageSelect = document.querySelector(".image-select");
+  const nextButton = document.querySelector(".button--register-next");
+  const title = document.querySelector(".title");
 
+  tokenInput.style.display = "none";
+  imageSelect.style.display = "none";
+  pincodeInput.style.display = "block";
+  nextButton.style.display = "block";
+  title.style.display = "block";
+
+ 
 });
