97aa3b53b84bf45ce090643a19f257fc80b3e9cb
mauroongena <mauro.ongena@icloud.com>
2025-06-06 10:27:38 +0200

    Added functionality to basic styling

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -11,5 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
   nextButton.style.display = "block";
   title.style.display = "block";
 
- 
+  pincodeInput.addEventListener("click", () => {
+    document.querySelector(".register__error-msg").innerHTML = "";
+  });
+  nextButton.addEventListener("click", () => {
+    if (pincodeInput.value.length === 4) {
+      imageSelect.style.display = "block";
+      pincodeInput.style.display = "none";
+      nextButton.style.display = "none";
+      title.style.display = "none";
+    } else {
+      document.querySelector(".register__error-msg").innerHTML =
+        "Pincode must be 4 digits long";
+    }
+  });
 });
