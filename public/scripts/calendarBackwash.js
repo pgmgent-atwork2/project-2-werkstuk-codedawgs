ea88f70ddba71528b58698d4f7b24b494ed2dba4
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-08 00:56:35 +0200

    Added basic posting function

--- public/scripts/calendarBackwash.js
+++ public/scripts/calendarBackwash.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
   async function fetchData(url, slug) {
     try {
       const response = await fetch(`${url}/api/${slug}`);
@@ -9,6 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
       return await response.json();
     } catch (error) {
       console.error(error.message);
+    }
+  }
+
+    async function postData(url, id, completed) {
+    try {
+      const response = await fetch(`${url}/tasks/${id}/completed`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          completed,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("error:", error.message);
     }
   }
 });
