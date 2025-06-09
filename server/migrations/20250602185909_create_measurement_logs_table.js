76bc5ab94ffda6c2431a423c8a1f20d49a89c195
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-09 16:40:59 +0200

    Updated logs migration and model

--- server/migrations/20250602185909_create_measurement_logs_table.js
+++ server/migrations/20250602185909_create_measurement_logs_table.js
@@ -4,13 +4,11 @@ export function up(knex) {
   return knex.schema.createTable(tableName, function (table) {
     table.increments("id").primary().unique();
     table.integer("user_id").unsigned().references("id").inTable("users");
-    table
-      .integer("sub_department_id")
-      .unsigned()
-      .references("id")
-      .inTable("sub_departments");
+    table.integer("sub_department_id").unsigned().references("id").inTable("sub_departments");
     table.date("measured_date").notNullable();
-    table.string("measurements");
+    table.integer("time").notNullable();
+    table.string("comment");
+    table.text("measurements");
   });
 }
 
