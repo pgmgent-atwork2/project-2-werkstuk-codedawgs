418a30a2389fac9bbb31b24a46b28dc414855c91
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-10 10:59:18 +0200

    Updated script to fetch definitions from API

--- public/scripts/analysis-add.js
+++ public/scripts/analysis-add.js
@@ -1,29 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const newButton = document.querySelector(".analysis__new");
-    const closeButton = document.querySelector(".analysis__close");
-    const newForm = document.querySelector(".analysis__form");
-    const inputs = document.querySelectorAll('input');
+document.addEventListener("DOMContentLoaded", async function () {
+  const URL = "http://localhost:3005";
 
-    newForm.style.display = "none"
+  async function fetchData(url, slug) {
+    try {
+      const response = await fetch(`${url}/api/${slug}`);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
-    newButton.addEventListener('click',() => {
-        newForm.style.display = "block"
-    })
-    closeButton.addEventListener('click',() => {
-        newForm.style.display = "none"
-        inputs.forEach(input => {
-            input.value = '';
-        });
-    })
+  // open and close form
+  const newButton = document.querySelector(".analysis__new");
+  const closeButton = document.querySelector(".analysis__close");
+  const newForm = document.querySelector(".analysis__form");
+  const inputs = document.querySelectorAll("input");
 
-    const freeCl = document.getElementById('4');
-    const totalCl = document.getElementById('5');
-    const combCl = document.getElementById('6');
+  newForm.style.display = "none";
 
-    totalCl.addEventListener('input', () => {
-        console.log('input');
-        
-        combCl.value = totalCl.value - freeCl.value
-    })
+  newButton.addEventListener("click", () => {
+    newForm.style.display = "block";
+  });
+  closeButton.addEventListener("click", () => {
+    newForm.style.display = "none";
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+  });
 
-})
+  //calc chlorine
+  const freeCl = document.getElementById("4");
+  const totalCl = document.getElementById("5");
+  const combCl = document.getElementById("6");
+
+  totalCl.addEventListener("input", () => {
+    combCl.value = totalCl.value - freeCl.value;
+  });
+
+  //calc min and max
+  const postAnalysis = document.querySelector(".analysis__add");
+  postAnalysis.addEventListener("click", () => {
+    console.log('test');
+    calcValues();
+  });
+
+  async function calcValues() {
+    const data = await fetchData(URL, "measurement-definitions");
+    inputs.forEach((input, index) => {
+        console.log(input.value, data[index].max_value);
+    });
+}
+});
--- server/controllers/api/measurementController.js
+++ server/controllers/api/measurementController.js
@@ -2,5 +2,7 @@ import MeasurementDefinitions from "../../models/MeasurementDefinition.js"
 
 export const measurementDefinitions = async (req, res, next) => {
     const measurementDefinitions = await MeasurementDefinitions.query();
+    console.log(measurementDefinitions);
+    
     res.json(measurementDefinitions);
 };
