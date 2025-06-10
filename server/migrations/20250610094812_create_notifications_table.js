45e0532928b72a661c4abff712fcf77df674a7d2
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-10 12:02:45 +0200

    Added notification migration

---
+++ server/migrations/20250610094812_create_notifications_table.js
@@ -0,0 +1,13 @@
+const tableName = "notification";
+ 
+export function up(knex) {
+    return knex.schema.createTable(tableName, function (table) {
+        table.increments("id").primary().unique();
+        table.integer("measurement_log_id").unsigned().references("id").inTable("measurement_logs");
+        table.integer("measurement_def_id").unsigned().references("id").inTable("measurement_definitions");
+    });
+}
+ 
+export function down(knex) {
+    return knex.schema.dropTable(tableName);
+}
