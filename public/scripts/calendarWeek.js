4c456e256368a23e804e458da5514d2104424379
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-11 15:09:51 +0200

    Added fetch tasklogs function

--- public/scripts/calendarWeek.js
+++ public/scripts/calendarWeek.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  async function fetchTaskLogs() {
+    try {
+      const response = await fetch(`api/tasklogs`);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const tasks = await response.json();
+      return tasks;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
-})
+});
