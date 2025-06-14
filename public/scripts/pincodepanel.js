95e900fca1d36ddd0dcad200ce59da79f6786f16
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-14 16:07:00 +0200

    Updated pincodepanel logic

--- public/scripts/pincodepanel.js
+++ public/scripts/pincodepanel.js
@@ -1,9 +1,10 @@
 const pincodeKeys = document.querySelectorAll(".pincode-panel__key");
 const input = document.querySelector(".login__pin-input");
-
 const pinInput = document.getElementById("pinInput");
 
 const pincodeDisplay = document.querySelectorAll(".pincode-display__circle");
+const pinError = document.getElementById('pinError');
+const userSelect = document.querySelector("#userSelect");
 
 let pinAmount = 0;
 
@@ -28,29 +29,23 @@ function updateDisplay() {
 
 pincodeKeys.forEach((key) => {
   key.addEventListener("click", () => {
-    document.querySelector(".login__error-msg").innerHTML = "";
+    pinError.innerHTML = "";
 
     if (key.classList.contains("pincode-panel__key--del")) {
       input.value = input.value.slice(0, -1);
-      pinAmount--;
-      if (pinAmount <= 0) {
-        pinAmount = 0;
-      }
+      pinInput.value = pinInput.value.slice(0, -1);
+      pinAmount = Math.max(0, pinAmount - 1);
     } else {
       input.value += key.dataset.id;
+      pinInput.value += key.dataset.id;
       pinAmount++;
     }
 
-    if (pinInput.value.length === 4) {
+    if (pinAmount === 4) {
       document.querySelector("form").requestSubmit();
     }
 
-    pincodeDisplay.forEach((display) => {
-      display.classList.remove("pincode-display__circle--filled");
-    });
-    for (let i = 0; i < pinAmount; i++) {
-      pincodeDisplay[i].classList.add("pincode-display__circle--filled");
-    }
+    updateDisplay();
     console.log(pinAmount);
   });
 });
@@ -62,35 +57,23 @@ document.addEventListener("keydown", (event) => {
 
     if (/^\d$/.test(key)) {
       if (pinAmount < 4) {
+        input.value += key;
         pinInput.value += key;
-        pincodeDisplay[pinAmount].classList.add(
-          "pincode-display__circle--filled"
-        );
         pinAmount++;
-
         if (pinAmount === 4) {
           document.querySelector("form").requestSubmit();
-        }
-
-        pincodeDisplay.forEach((display) => {
-          display.classList.remove("pincode-display__circle--filled");
-        });
-        for (let i = 0; i < pinAmount; i++) {
-          pincodeDisplay[i].classList.add("pincode-display__circle--filled");
         }
       }
     } else if (key === "Backspace") {
       if (pinAmount > 0) {
-        pinAmount--;
+        input.value = input.value.slice(0, -1);
         pinInput.value = pinInput.value.slice(0, -1);
-        pincodeDisplay[pinAmount].classList.remove(
-          "pincode-display__circle--filled"
-        );
+        pinAmount--;
       }
     }
+    updateDisplay();
+    console.log(pinAmount);
   }
-
-  console.log(pinAmount);
 });
 
-const delKey = document.querySelector(".pincode-panel__key--del");
+window.resetPin = resetPin;
