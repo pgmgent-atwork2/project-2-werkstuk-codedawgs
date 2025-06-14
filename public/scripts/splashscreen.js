04d634d1adc756b761eb8d661c4a479f64a43ab4
mauroongena <mauro.ongena@icloud.com>
2025-06-14 22:25:13 +0200

    Removed overcooked script

--- public/scripts/splashscreen.js
+++ public/scripts/splashscreen.js
@@ -1,10 +1,8 @@
 window.addEventListener("load", () => {
     const splash = document.getElementById("splashscreen");
-    const loginSection = document.getElementById("loginSection");
     if (splash) {
         setTimeout(() => {
             splash.classList.add("hidden");
-            loginSection.classList.add("hidden")
             setTimeout(() => splash.remove(), 500);
         }, 5000); 
     }
