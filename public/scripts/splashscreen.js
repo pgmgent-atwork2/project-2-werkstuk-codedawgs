04eeb64cc754e1145d32d9bcf983c3017b7694fa
mauroongena <mauro.ongena@icloud.com>
2025-06-14 21:25:16 +0200

    Added script to splashscreen

---
+++ public/scripts/splashscreen.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+    const splash = document.getElementById("splashscreen");
+    if (splash) {
+        setTimeout(() => {
+            splash.classList.add("hidden");
+            setTimeout(() => splash.remove(), 500);
+        }, 3000);
+    }
+});
