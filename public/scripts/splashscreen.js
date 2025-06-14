85656bc7cb351b570176828766f7786bc4dddb45
mauroongena <mauro.ongena@icloud.com>
2025-06-14 22:24:25 +0200

    Added media query and bug fix

--- public/scripts/splashscreen.js
+++ public/scripts/splashscreen.js
@@ -1,9 +1,11 @@
 window.addEventListener("load", () => {
     const splash = document.getElementById("splashscreen");
+    const loginSection = document.getElementById("loginSection");
     if (splash) {
         setTimeout(() => {
             splash.classList.add("hidden");
+            loginSection.classList.add("hidden")
             setTimeout(() => splash.remove(), 500);
-        }, 3000);
+        }, 5000); 
     }
 });
