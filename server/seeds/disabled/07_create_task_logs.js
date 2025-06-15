9ec59af309afeeef0981346bf6cd450860d15320
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-15 14:12:39 +0200

    Updated task log seed

--- server/seeds/disabled/07_create_task_logs.js
+++ server/seeds/disabled/07_create_task_logs.js
@@ -4,10 +4,10 @@ const seed = async function (knex) {
   await knex(tableName).truncate();
   await knex(tableName).insert([{ 
     user_id: 2,
-    task_id: 4,
+    task_id: 2,
     object_type: "pump",
-    object_id: 4,
-    comment: "checked pump very good",
+    object_id: 2,
+    comment: "checked pump",
     action: "completed",
     task_date: new Date("2025-04-17T03:24:00")
   },
